57c78f5312633975b20ccbe4e8e0d83d9b2c2e78
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-04-14 17:36:12 +0200

    Suchfelder implementiert (Nutzername, Freunde und Chat suchen)

---
+++ components/SearchField.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { TextInput } from "react-native-gesture-handler";
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+type Props = {
+  label: string;
+};
+
+export default function SearchField({ label }: Props) {
+
+    return (
+        <GestureHandlerRootView style={styles.inputContainer}>
+            <AntDesign name="search1" size={20} style={styles.icon} />
+            <TextInput
+            style={styles.input}
+            autoCapitalize='none'
+            autoCorrect={false}
+            inputMode='text'
+            maxLength={20}
+            placeholder={label}
+            placeholderTextColor={"#E5E9F0"}
+            returnKeyType='done'
+            selectionColor={"#30C5FF"}>
+            </TextInput>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        height: 46,
+        width: 360,
+        flexDirection: 'row',
+        borderRadius: 10,
+        backgroundColor: '#4A5160',
+    },
+    icon: {
+        top: 12,
+        left: 10,
+        marginRight: 20,
+        color: "#30C5FF",
+    },
+    input: {
+        color: '#E5E9F0',
+        fontSize: 16,
+        width: '87.5%',
+        
+      },
+});
