ac24827854e4fdc715514bae7a369b3c660c425e
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-04-20 17:39:47 +0200

    Bei der Not Found Page den Titel im Header entfernt

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" options={{ title: 'Profil' }} />
+      <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
