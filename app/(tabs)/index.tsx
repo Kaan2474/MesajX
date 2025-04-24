859214cf1e18982ec7568636bcd2fe5faeeada85
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-04-24 16:11:25 +0200

    Chats Page implementiert

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,165 @@
+import React from 'react';
+import { Text, View, StyleSheet, Platform, FlatList } from "react-native";
+import { Link } from "expo-router";
+import Button from '@/components/Button';
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import SearchField from '@/components/SearchField';
+import ButtonWithIcon from '@/components/ButtonWithIcon';
+import InputField from '@/components/InputField';
+import ProfilePicture from '@/components/ProfilePicture';
+import Logo from '@/components/Logo';
+import ChatContainer from '@/components/ChatContainer';
+
+const PlaceholderImage = require('@/assets/images/Logo.png');
+
+const DATA = [
+  {
+    id: '1',
+    name: 'Kaan Hisiroglu',
+    message: "Hallo, wie geht es dir?",
+    time: "22:17",
+    unreadMessages: "2",
+    isOnline: true,
+    initials: "KH"
+  },
+  {
+    id: '2',
+    name: 'Alkan Cinar',
+    message: "Kommst du morgen?",
+    time: "19:47",
+    unreadMessages: "3",
+    isOnline: false,
+    initials: "AC"
+  },
+  {
+    id: '3',
+    name: 'Oktay Cetin',
+    message: "Ey ich muss dir was erzählen",
+    time: "gestern",
+    unreadMessages: "2",
+    isOnline: true,
+    initials: "OC"
+  },
+  {
+    id: '4',
+    name: 'Mahmoud Ali Khan',
+    message: "Ich habe große Neuigkeiten",
+    time: "Montag",
+    unreadMessages: "4",
+    isOnline: true,
+    initials: "MA"
+  },
+  {
+    id: '5',
+    name: 'Karim El Kourouchi',
+    message: "Kommst du mit nach Marokko?",
+    time: "Sonntag",
+    unreadMessages: "10",
+    isOnline: true,
+    initials: "KE"
+  },
+  {
+    id: '6',
+    name: 'Oguzhan Akgün',
+    message: "Training morgen?",
+    time: "Samstag",
+    unreadMessages: "7",
+    isOnline: false,
+    initials: "OG"
+  },
+  {
+    id: '7',
+    name: 'Mikail Akgün',
+    message: "Bist du noch krank?",
+    time: "Freitag",
+    unreadMessages: "1",
+    isOnline: true,
+    initials: "MA"
+  },
+  {
+    id: '8',
+    name: 'Lars Daumann',
+    message: "Habe deine Bewerbung abgeg...",
+    time: "Donnerstag",
+    unreadMessages: "5",
+    isOnline: false,
+    initials: "LD"
+  },
+  {
+    id: '9',
+    name: 'Dikran Artin',
+    message: "Kannst du meine Aufgabe mac...",
+    time: "Mittwoch",
+    unreadMessages: "2",
+    isOnline: false,
+    initials: "DA"
+  },
+  {
+    id: '10',
+    name: 'Selim Tutgun',
+    message: "Ich heirate am 20.05",
+    time: "Dienstag",
+    unreadMessages: "1",
+    isOnline: true,
+    initials: "ST"
+  },
+];
+
+export default function Index() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+
+        <View>
+          <Logo imgSource={PlaceholderImage}/>
+        </View>
+
+      <View style={styles.searchBarAndButtonWithIconContainer}>
+        <SearchField label="Suche Chat..." />
+        <ButtonWithIcon />
+      </View>
+
+    <View style={{alignItems: "center"}}>
+    <FlatList
+        data={DATA}
+        renderItem={({item}) => <ChatContainer name={item.name} time={item.time} message={item.message} unreadMessages={item.unreadMessages} isOnline={item.isOnline} initials={item.initials}/>}
+        keyExtractor={item => item.id}
+      />
+    </View>
+
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1F2227',
+    alignItems: "center",
+  },
+  searchBarAndButtonWithIconContainer: {
+    marginTop: 25,
+    flexDirection: "row",
+    width: 360,
+    justifyContent: "space-between"
+  },
+  time: {
+    fontSize: 16,
+    color: "#E5E9F0",
+    top: 10
+  },
+  title: {
+    fontSize: 20,
+    color: "#E5E9F0",
+    bottom: 10
+  },
+  message: {
+    fontSize: 16,
+    color: "#E5E9F0"
+  },
+  unreadMessages: {
+    fontSize: 16,
+    color: "#fff",
+  }
+});
