a39863a4874fcb9306de9c0e90a76d1b3398a5f5
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-04-25 16:49:30 +0200

    Komponente für Freundesliste: Stellt einen Eintrag in der Freundesliste dar

---
+++ components/FriendContainer.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, View, Text } from 'react-native';
+import ProfilePicture from '@/components/ProfilePicture';
+
+type Props = {
+    initials: string,
+    isOnline: boolean,
+    name: string,
+};
+
+export default function FriendContainer({ initials, isOnline, name } : Props) {
+
+  return (
+    <View style={styles.item}>
+
+        <ProfilePicture initials={initials} isOnline={isOnline}/>
+        <View style={styles.nameContainer}>
+            <Text style={styles.name}>{name}</Text>
+        </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+    width: 360,
+    height: 70,
+    borderBottomWidth: 1,
+    borderBottomColor: "#4A5160",
+    marginTop: 10,
+    alignItems: "center",
+  },
+  nameContainer: {
+    left: 30
+  },
+  name: {
+    fontSize: 16,
+    color: "#E5E9F0",
+  },
+});
