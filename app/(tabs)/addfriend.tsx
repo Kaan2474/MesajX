69aebb6e2bcdc79e76243941f8c5ca3bf49369bc
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-04-27 17:11:23 +0200

    Freund hinzufügen Page implementiert

--- app/(tabs)/addfriend.tsx
+++ app/(tabs)/addfriend.tsx
@@ -1,21 +1,76 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import Logo from '@/components/Logo';
+import SearchField from '@/components/SearchField';
+import Button from '@/components/Button';
 
-export default function Profile() {
+const PlaceholderImage = require('@/assets/images/Logo.png');
+
+export default function AddFriend() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Freund hinzufügen</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+
+        <Logo imgSource={PlaceholderImage}/>
+
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.descriptionHeading}>Finde neue Freunde!</Text>
+          <Text style={styles.descriptionText}>Suche nach anderen Nutzern und sende Freundschaftsanfragen, um gemeinsam zu chatten</Text>
+        </View>
+
+        <View style={styles.searchBarContainer}>
+          <SearchField label="Nutzername"/>
+        </View>
+
+        <Button label="Freund hinzufügen"/>
+
+        
+        <Text style={styles.friendRequestTextContainer}>
+          <Text style={styles.friendRequestsText}>Tippe </Text>
+          <Text style={[styles.friendRequestsText, {fontWeight: 700}]}>hier </Text>
+          <Text style={styles.friendRequestsText}>um deine Freundschaftsanfragen zu sehen</Text>
+        </Text>
+
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#1F2227',
+    alignItems: "center",
   },
-  text: {
-    color: '#fff',
+  descriptionContainer: {
+    height: 125,
+    width: 300,
+    justifyContent: "space-between",
+    marginTop: 40,
+  },
+  descriptionHeading: {
+    fontSize: 22,
+    fontWeight: "700",
+    textAlign: "center",
+    color: "#E5E9F0",
+  },
+  descriptionText: {
+    fontSize: 14,
+    textAlign: "center",
+    color: "#E5E9F0"
+  },
+  searchBarContainer: {
+    marginTop: 40,
+    marginBottom: 15
+  },
+  friendRequestTextContainer: {
+    width: 250,
+    position: "absolute",
+    bottom: 50,
+  },
+  friendRequestsText: {
+    color: "#E5E9F0",
+    textAlign: "center", 
+    fontSize: 16
   },
 });
