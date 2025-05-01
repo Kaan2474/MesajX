e493097223e05e569b3136deef76ea16ebb1d539
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-05-01 14:39:33 +0200

    Page für Freundschaftsanfragen implementiert

---
+++ app/friendRequests.tsx
@@ -0,0 +1,107 @@
+import { Text, View, StyleSheet, Pressable, FlatList } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Logo from '@/components/Logo';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useRouter } from 'expo-router';
+import ProfilePicture from '@/components/ProfilePicture';
+import FriendRequestContainer from '@/components/friendRequestContainer';
+
+const DATA = [
+  {
+    id: '1',
+    name: 'Kaan Hisiroglu',
+    initials: "KH"
+  },
+  {
+    id: '2',
+    name: 'Alkan Cinar',
+    initials: "AC"
+  },
+  {
+    id: '3',
+    name: 'Oktay Cetin',
+    initials: "OC"
+  },
+  {
+    id: '4',
+    name: 'Mahmoud Ali Khan',
+    initials: "MA"
+  },
+  {
+    id: '5',
+    name: 'Karim El Kourouchi',
+    initials: "KE"
+  },
+  {
+    id: '6',
+    name: 'Oguzhan Akgün',
+    initials: "OG"
+  },
+  {
+    id: '7',
+    name: 'Mikail Akgün',
+    initials: "MA"
+  },
+  {
+    id: '8',
+    name: 'Lars Daumann',
+    initials: "LD"
+  },
+  {
+    id: '9',
+    name: 'Dikran Artin',
+    initials: "DA"
+  },
+  {
+    id: '10',
+    name: 'Selim Tutgun',
+    initials: "ST"
+  },
+];
+
+export default function FriendRequests() {
+
+const ROUTER = useRouter();
+
+  return (
+    <SafeAreaProvider>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.logoAndBackbuttonContainer}>
+                <View style={styles.buttonContainer}>
+                    <Pressable onPress={() => ROUTER.navigate("/(tabs)/addfriend")}>
+                        <Ionicons name="chevron-back" size={30} color="#30C5FF" />
+                    </Pressable>
+                </View>
+
+                <Logo />
+            </View>
+
+            <FlatList
+            data={DATA}
+            renderItem={({item}) => <FriendRequestContainer initials={item.initials} name={item.name}/>}
+            keyExtractor={item => item.id}
+            />
+
+        </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1F2227',
+    alignItems: "center"
+  },
+  logoAndBackbuttonContainer: {
+    flexDirection: "row",
+    marginTop: 45,
+    width: 360,
+    justifyContent: "center"
+  },
+  buttonContainer: {
+    position: "absolute",
+    left: 7.5,
+    bottom: 25
+  },
+});
