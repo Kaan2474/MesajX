88d62547394f3133561aaf223ed47ecdf078d9b1
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-05-04 16:19:21 +0200

    Der Header wurde zuvor zweimal entfernt und der Header wird nun nur noch einmal entfernt

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout() {
         headerShown: false,
       }}
     >
-      <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+      <Stack.Screen name="(tabs)"/>
       <Stack.Screen name="+not-found" />
     </Stack>
   );
