d33fefdfd46bf347a2b0bf5e9b5fccd02729418a
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-05-04 17:24:14 +0200

    Profil von Freunden implementiert

---
+++ app/friendProfile.tsx
@@ -0,0 +1,96 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Logo from '@/components/Logo';
+import ProfilePicture from '@/components/ProfilePicture';
+import SmallButton from '@/components/SmallButton';
+
+export default function FriendProfile() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+
+        <Logo />
+
+        <View style={styles.profilepictureNamesDescriptionContainer}>
+          <ProfilePicture size="big" initials={"AC"} />
+          <View style={styles.namesContainer}>
+            <Text style={styles.name}>Alkan Cinar</Text>
+            <Text style={styles.username}>killeralkan6767</Text>
+          </View>
+
+          <View style={styles.descriptionContainer}>
+            <Text style={styles.profileDescription}>Hallo, mein Name ist Alkan und meine größte Leidenschaft ist das Boxen! Ich habe bereits 4 Profikämpfe absolviert</Text>
+          </View>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <SmallButton label={"Chatten"}/>
+          <SmallButton label={"Entfernen"}/>
+        </View>
+
+        <View style={styles.friendsAndMembershipContainer}>
+          <View>
+            <Text style={styles.friendsAndMembershipText}>2</Text>
+            <Text style={styles.friendsAndMembershipText}>Freunde</Text>
+          </View>
+          <View>
+            <Text style={styles.friendsAndMembershipText}>Mitglied seit</Text>
+            <Text style={styles.friendsAndMembershipText}>01.10.2026</Text>
+          </View>
+        </View>
+
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1F2227',
+    alignItems: 'center',
+  },
+  profilepictureNamesDescriptionContainer: {
+    alignItems: "center",
+    marginTop: 50,
+  },
+  namesContainer: {
+    alignItems: "center",
+    top: 10
+  },
+  name: {
+    color: "#E5E9F0",
+    fontSize: 16,
+  },
+  username: {
+    color: "#E5E9F0",
+    top: 5
+  },
+  descriptionContainer: {
+    marginTop: 75,
+    alignItems: "center",
+    width: 300
+  },
+  profileDescription: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "#E5E9F0"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 75,
+    width: 355,
+    justifyContent: "space-around",
+  },
+  friendsAndMembershipContainer: {
+    flexDirection: "row",
+    width: 355,
+    marginTop: 50,
+    justifyContent: "space-around",
+  },
+  friendsAndMembershipText: {
+    color: "#E5E9F0",
+    fontSize: 18,
+    textAlign: "center"
+  }
+});
