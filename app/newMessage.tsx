4f9bd14321a7d544cd479f511a0d2f07f5a48912
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-05-05 20:34:05 +0200

    Page für ,,neue Nachricht senden'' implementiert

---
+++ app/newMessage.tsx
@@ -0,0 +1,89 @@
+import { View, StyleSheet, FlatList } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Logo from '@/components/Logo';
+import SearchField from '@/components/SearchField';
+import NewMessageContainer from '@/components/NewMessageContainer';
+
+const DATA = [
+  {
+    id: '1',
+    name: 'Kaan Hisiroglu',
+    status: "online",
+    initials: "KH"
+  },
+  {
+    id: '2',
+    name: 'Alkan Cinar',
+    status: "offline",
+    initials: "AC"
+  },
+  {
+    id: '3',
+    name: 'Lars Daumann',
+    status: "offline",
+    initials: "LD"
+  },
+  {
+    id: '4',
+    name: 'Dikran Artin',
+    status: "online",
+    initials: "DA"
+  },
+  {
+    id: '5',
+    name: 'Can Akgün',
+    status: "offline",
+    initials: "CA"
+  },
+  {
+    id: '6',
+    name: 'Luca Quadrelli',
+    status: "offline",
+    initials: "LQ"
+  },
+  {
+    id: '7',
+    name: 'Mahmoud Ali Khan',
+    status: "online",
+    initials: "MA"
+  },
+  {
+    id: '8',
+    name: 'Karim El Kourouchi',
+    status: "online",
+    initials: "KE"
+  },
+]
+
+export default function NewMessage() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+
+        <Logo withBackButton={true}/>
+
+        <View style={styles.searchBarContainer}>
+          <SearchField label="Suche Freunde..." />
+        </View>
+
+        <FlatList
+          data={DATA}
+          renderItem={({item}) => <NewMessageContainer name={item.name} status={item.status} initials={item.initials}/>}
+          keyExtractor={item => item.id}
+        />
+
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1F2227',
+    alignItems: 'center',
+  },
+  searchBarContainer: {
+    marginTop: 25,
+  },
+});
