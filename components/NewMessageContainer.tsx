48e92645aa57fd88b7c14021a64c7741c86adc62
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-05-05 20:35:41 +0200

    Komponente für einen Eintrag in der ,,neue Nachricht senden'' Page implementiert

---
+++ components/NewMessageContainer.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, View, Text } from 'react-native';
+import ProfilePicture from '@/components/ProfilePicture';
+import { useRouter } from 'expo-router';
+import SmallButton from './SmallButton';
+
+type Props = {
+    initials: string,
+    status: string,
+    name: string,
+};
+
+const ROUTER = useRouter();
+
+export default function NewMessageContainer({ initials, status, name } : Props) {
+
+  return (
+    <View style={styles.item}>
+      <ProfilePicture status={status} size={"small"} initials={initials}/>
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{name}</Text>
+      </View>
+      <View style={styles.chatButtonContainer}>
+        <SmallButton />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    width: 360,
+    height: 80,
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#4A5160",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  nameContainer: {
+    left: 30,
+    width: 175,
+  },
+  name: {
+    fontSize: 16,
+    color: "#E5E9F0",
+  },
+  chatButtonContainer: {
+    position: "absolute",
+    right: 10
+  },
+});
