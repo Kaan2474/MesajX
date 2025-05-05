52a71e721c494488844d1e811aef07c74e1dc749
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-05-05 19:52:46 +0200

    Unnötige imports entfernt und der Button mit dem Icon navigiert nun zu der Page newMessage.tsx

--- components/ButtonWithIcon.tsx
+++ components/ButtonWithIcon.tsx
@@ -1,11 +1,14 @@
-import { StyleSheet, View, Pressable, Text } from 'react-native';
+import { StyleSheet, View, Pressable } from 'react-native';
 import Entypo from '@expo/vector-icons/Entypo';
+import { useRouter } from 'expo-router';
 
 export default function ButtonWithIcon() {
 
+  const ROUTER = useRouter();
+
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Pressable style={styles.button} onPress={() => ROUTER.navigate("/newMessage")}>
         <Entypo name="new-message" size={24} color="#30C5FF" />
       </Pressable>
     </View>
