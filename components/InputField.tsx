83c677368394499d08fafddb7df60977599cdb3a
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-05-09 17:09:53 +0200

    Alle Arten von Eingabefeldern in eine einzige Komponente zusammengefasst

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -5,126 +5,60 @@ import { TextInput } from "react-native-gesture-handler";
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 type Props = {
   type: string;
-  label: string;
+  placeholder: string;
+  inputFieldHeight: number;
+  inputFieldWidth: number;
 };
 
-export default function InputField({ type, label }: Props) {
+//Normale Eingabefelder --> Höhe: 40 und Breite: 360
+//Eingabefeld für Beschreibung --> Höhe: 55 und Breite: 360
+//Eingabefeld mit Button daneben --> Höhe: 40 und Breite: 303.5
 
-    if (type === "Nutzername") {
-        return (
-            <GestureHandlerRootView style={styles.inputContainer}>
-                <View style={styles.iconContainer}>
-                    <FontAwesome name="user-circle-o" size={18} color="#30C5FF" />
-                </View>
-                <TextInput
-                style={styles.input}
-                autoCapitalize='none'
-                autoCorrect={false}
-                inputMode='text'
-                maxLength={20}
-                placeholder={label}
-                placeholderTextColor={"#E5E9F0"}
-                returnKeyType='done'
-                selectionColor={"#30C5FF"}>
-                </TextInput>
-            </GestureHandlerRootView>
-      );
-    }
+export default function InputField({ inputFieldHeight, inputFieldWidth, type, placeholder }: Props) {
 
-    else if (type === "Passwort" || type === "Passwort bestätigen") {
-        return (
-            <GestureHandlerRootView style={styles.inputContainer}>
-                <View style={styles.iconContainer}>
-                    <FontAwesome6 name="lock" size={18} color={"#30C5FF"} />
-                </View>
-                <TextInput
-                style={styles.input}
-                inputMode='text'
-                placeholder={label}
-                placeholderTextColor={"#E5E9F0"}
-                returnKeyType='done'
-                secureTextEntry={true}
-                selectionColor={"#30C5FF"}>
-                </TextInput>
-            </GestureHandlerRootView>
-      );
-    }
+  return (
+    <GestureHandlerRootView style={[styles.inputContainer, {height: inputFieldHeight}, {width: inputFieldWidth}]}>
+        <View style={styles.iconContainer}>
+          {!! (type === "Nutzername") && <FontAwesome name="user-circle-o" size={18} color="#30C5FF" />}
+          {!! (type === "Passwort" || type ==="Passwort bestätigen") && <FontAwesome6 name="lock" size={18} color={"#30C5FF"} />}
+          {!! (type === "Vorname" || type ==="Nachname") && <FontAwesome name="user" size={18} color={"#30C5FF"} />}
+          {!! (type === "Email") && <MaterialIcons name="email" size={18} color={"#30C5FF"} />}
+          {!! (type === "Suchen") && <AntDesign name="search1" size={18} color={"#30C5FF"} />}
+          {!! (type === "Beschreibung") && <MaterialIcons name="description" size={18} color={"#30C5FF"} />}
+        </View>
+    
+        {!! (type === "Nutzername") && <TextInput style={styles.input} autoCapitalize='none' autoCorrect={false} inputMode='text'
+        maxLength={20} placeholder={placeholder} placeholderTextColor={"#E5E9F0"} returnKeyType='done'
+        selectionColor={"#30C5FF"} />}
 
-    else if (type === "Vorname" || type === "Nachname") {
-        return (
-            <GestureHandlerRootView style={styles.inputContainer}>
-                <View style={styles.iconContainer}>
-                    <FontAwesome name="user" size={18} color={"#30C5FF"} />
-                </View>
-                <TextInput
-                style={styles.input}
-                autoCorrect={false}
-                inputMode='text'
-                maxLength={40}
-                placeholder={label}
-                placeholderTextColor={"#E5E9F0"}
-                returnKeyType='done'
-                selectionColor={"#30C5FF"}>
-                </TextInput>
-            </GestureHandlerRootView>
-      );
-    }
+        {!! (type === "Passwort" || type ==="Passwort bestätigen") && <TextInput style={styles.input} inputMode='text'
+        placeholder={placeholder} placeholderTextColor={"#E5E9F0"} returnKeyType='done' secureTextEntry={true}
+        selectionColor={"#30C5FF"} />}
 
-    else if (type === "Email") {
-        return (
-            <GestureHandlerRootView style={styles.inputContainer}>
-                <View style={styles.iconContainer}>
-                    <MaterialIcons name="email" size={18} color={"#30C5FF"} />
-                </View>
-                <TextInput
-                style={styles.input}
-                autoCapitalize='none'
-                autoCorrect={false}
-                inputMode='email'
-                placeholder={label}
-                placeholderTextColor={"#E5E9F0"}
-                returnKeyType='done'
-                selectionColor={"#30C5FF"}>
-                </TextInput>
-            </GestureHandlerRootView>
-      );
-    }
+        {!! (type === "Vorname" || type ==="Nachname") && <TextInput style={styles.input} autoCorrect={false} inputMode='text'
+        maxLength={40} placeholder={placeholder} placeholderTextColor={"#E5E9F0"} returnKeyType='done'
+        selectionColor={"#30C5FF"} />}
 
-    else if (type === "Beschreibung") {
-        return (
-            <GestureHandlerRootView style={[styles.inputContainer, {height: 55}]}>
-                <View style={styles.iconContainer}>
-                    <MaterialIcons name="description" size={18} color={"#30C5FF"} />
-                </View>
-                <TextInput
-                style={styles.input}
-                autoCorrect={false}
-                inputMode='text'
-                maxLength={85}
-                multiline={true}
-                placeholder={label}
-                placeholderTextColor={"#E5E9F0"}
-                returnKeyType='done'
-                selectionColor={"#30C5FF"}>
-                </TextInput>
-            </GestureHandlerRootView>
-      );
-    }
+        {!! (type === "Email") && <TextInput style={styles.input} autoCapitalize='none' autoCorrect={false}
+        inputMode='email' placeholder={placeholder} placeholderTextColor={"#E5E9F0"} returnKeyType='done' 
+        selectionColor={"#30C5FF"} />}
 
-    else {
-        return (
-            <></>
-        )
-    }
+        {!! (type === "Suchen") && <TextInput style={styles.input} autoCapitalize='none' autoCorrect={false} inputMode='text'
+        placeholder={placeholder} placeholderTextColor={"#E5E9F0"} returnKeyType='done' selectionColor={"#30C5FF"} />}
+
+        {!! (type === "Beschreibung") && <TextInput style={styles.input} autoCorrect={false} inputMode='text' maxLength={85} multiline={true}
+        placeholder={placeholder} placeholderTextColor={"#E5E9F0"} returnKeyType='done' selectionColor={"#30C5FF"} />}
+    
+    </GestureHandlerRootView>
+  );
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-        height: 40,
-        width: 360,
         flexDirection: 'row',
         borderRadius: 10,
         backgroundColor: '#4A5160',
