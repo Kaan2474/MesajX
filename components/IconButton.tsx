54c0c4893dcca1decf9a871eca4209a0e6950207
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-05-11 13:17:14 +0200

    Komponente ButtonWithIcon.tsx zu IconButton.tsx umbenannt

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -7,7 +7,7 @@ type Props = {
   functionality: void;
 }
 
-export default function ButtonWithIcon({ type, functionality }: Props) {
+export default function IconButton({ type, functionality }: Props) {
   <Pressable style={styles.button} onPress={() => functionality}>
     {!! (type === "Neuer Chat") && <Entypo name="new-message" size={24} color="#30C5FF"/>}
     {!! (type === "Nachricht senden") && <FontAwesome name="send" size={24} color="#30C5FF"/>}
