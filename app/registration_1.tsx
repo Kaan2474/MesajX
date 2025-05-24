f715512901f6fcf8ca4f4accb784c5a6f092bd87
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-05-24 15:22:58 +0200

    Responsive, konsistente Attribute hinzugefügt

--- app/registration_1.tsx
+++ app/registration_1.tsx
@@ -7,44 +7,41 @@ import { useRouter } from 'expo-router';
 import BigButton from '@/components/BigButton';
 import { Link } from 'expo-router';
 import ProgressBar from '@/components/ProgressBar';
+import colors from './utils/colors';
+import universalStyles from './utils/universalStyles';
 
 const { height, width } = Dimensions.get("window");
-const scale = width / 320;
 
 const inputFieldAndButtonHeight = height * (1/21.1);
 const inputFieldAndButtonWidth = width * (12/13);
 
-const scaleFontSize = (size: number) => {
-  const newSize = size * scale;
-  return Math.round(PixelRatio.roundToNearestPixel(newSize))
-}
 
 export default function Registration_1() {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView style={universalStyles.styles.container}>
 
         <Logo withBackButton={false}/>
 
-        <View style={styles.headingDescriptionContainer}>
-            <Text style={styles.heading}>Registriere dich bei MesajX!</Text>
-            <Text style={styles.description}>Erstelle in wenigen Schritten dein Konto und starte deine Unterhaltung mit Freunden und Familie.</Text>
+        <View style={universalStyles.styles.headingContainer}>
+            <Text style={universalStyles.styles.headingText}>Registriere dich bei MesajX!</Text>
+            <Text style={universalStyles.styles.descriptionText}>Erstelle in wenigen Schritten dein Konto und starte deine Unterhaltung mit Freunden und Familie.</Text>
         </View>
 
-        <View style={styles.inputFieldContainer}>
+        <View style={universalStyles.styles.inputFieldMargin}>
             <InputField inputFieldHeight={inputFieldAndButtonHeight} inputFieldWidth={inputFieldAndButtonWidth} type='Vorname' placeholder='Vorname' />
         </View>
-        <View style={styles.inputFieldContainer}>
+        <View style={universalStyles.styles.inputFieldMargin}>
             <InputField inputFieldHeight={inputFieldAndButtonHeight} inputFieldWidth={inputFieldAndButtonWidth} type='Nachname' placeholder='Nachname' />
         </View>
-        <View style={styles.inputFieldContainer}>
+        <View style={universalStyles.styles.inputFieldMargin}>
             <InputField inputFieldHeight={inputFieldAndButtonHeight} inputFieldWidth={inputFieldAndButtonWidth} type='Email' placeholder='Email' />
         </View>
-        <View style={styles.inputFieldContainer}>
+        <View style={universalStyles.styles.inputFieldMargin}>
             <InputField inputFieldHeight={inputFieldAndButtonHeight} inputFieldWidth={inputFieldAndButtonWidth} type='Nutzername' placeholder='Nutzername' />
         </View>
-        <View style={styles.inputFieldContainer}>
+        <View style={universalStyles.styles.inputFieldMargin}>
             <InputField inputFieldHeight={inputFieldAndButtonHeight} inputFieldWidth={inputFieldAndButtonWidth} type='Passwort' placeholder='Passwort' />
         </View>
 
@@ -59,36 +56,10 @@ export default function Registration_1() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1F2227',
-    alignItems: "center",
-  },
-  headingDescriptionContainer: {
-    height: "20%",
-    width: "100%",
-    justifyContent: "space-around",
-    marginTop: height * 1/21.1,
-    marginBottom: height * 1/12.98461538,
-  },
-  heading: {
-    color: "#E5E9F0",
-    fontSize: scaleFontSize(18.051282205),
-    fontWeight: "700",
-    textAlign: "center"
-  },
-  description: {
-    color: "#E5E9F0",
-    textAlign: "center",
-    fontSize: scaleFontSize(11.48717949)
-  },
-  inputFieldContainer: {
-    marginBottom: height * 1/84.4
-  },
   alreadyHaveAnAccText: {
-    color: "#E5E9F0",
+    color: colors.textColor,
     textAlign: "center",
-    top: height * 1/42.2
+    top: universalStyles.SPACE_AFTER_CONTENT
   },
   loginText: {
     fontWeight: "700"
