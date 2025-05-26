6e62f49d1b2b91f7b4a07dc8ddc70b586efd0d61
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-05-26 19:09:17 +0200

    Höhe und Breite der Eingabefelder werden dynamisch übergeben und globale Farbe für die Farbe des Eingabefelds implementiert

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { TextInput } from "react-native-gesture-handler";
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
@@ -7,6 +7,7 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import colors from '@/app/utils/colors';
 
 type Props = {
   type: string;
@@ -19,18 +20,18 @@ type Props = {
 //Eingabefeld für Beschreibung --> Höhe: 55 und Breite: 360
 //Eingabefeld mit Button daneben --> Höhe: 40 und Breite: 303.5
 
-export default function InputField({ inputFieldHeight, inputFieldWidth, type, placeholder }: Props) {
+export default function InputField({ type, placeholder, inputFieldHeight, inputFieldWidth }: Props) {
 
   return (
     <GestureHandlerRootView style={[styles.inputContainer, {height: inputFieldHeight}, {width: inputFieldWidth}]}>
         <View style={styles.iconContainer}>
-          {!! (type === "Nutzername") && <FontAwesome name="user-circle-o" size={18} color="#30C5FF" />}
-          {!! (type === "Passwort" || type ==="Passwort bestätigen") && <FontAwesome6 name="lock" size={18} color={"#30C5FF"} />}
-          {!! (type === "Vorname" || type ==="Nachname") && <FontAwesome name="user" size={18} color={"#30C5FF"} />}
-          {!! (type === "Email") && <MaterialIcons name="email" size={18} color={"#30C5FF"} />}
-          {!! (type === "Suchen") && <AntDesign name="search1" size={18} color={"#30C5FF"} />}
-          {!! (type === "Beschreibung") && <MaterialIcons name="description" size={18} color={"#30C5FF"} />}
-          {!! (type === "Chat") && <Ionicons name="chatbubble-ellipses" size={18} color="#30C5FF" />}
+          {!! (type === "Vorname" || type ==="Nachname") && <FontAwesome name="user" size={18} color={colors.inputFieldIconColor} />}
+          {!! (type === "Email") && <MaterialIcons name="email" size={18} color={colors.inputFieldIconColor} />}
+          {!! (type === "Nutzername") && <FontAwesome name="user-circle-o" size={18} color={colors.inputFieldIconColor} />}
+          {!! (type === "Passwort" || type ==="Passwort bestätigen") && <FontAwesome6 name="lock" size={18} color={colors.inputFieldIconColor} />}
+          {!! (type === "Suchen") && <AntDesign name="search1" size={18} color={colors.inputFieldIconColor} />}
+          {!! (type === "Beschreibung") && <MaterialIcons name="description" size={18} color={colors.inputFieldIconColor} />}
+          {!! (type === "Chat") && <Ionicons name="chatbubble-ellipses" size={18} color={colors.inputFieldIconColor} />}
         </View>
     
         {!! (type === "Nutzername") && <TextInput style={styles.input} autoCapitalize='none' autoCorrect={false} inputMode='text'
@@ -66,7 +67,7 @@ const styles = StyleSheet.create({
     inputContainer: {
         flexDirection: 'row',
         borderRadius: 10,
-        backgroundColor: '#4A5160',
+        backgroundColor: colors.inputFieldColor,
         alignItems: "center"
     },
     iconContainer: {
