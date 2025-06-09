e6de9d5f47e0e1677f9d5ad9cfef270239da17e8
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-06-09 19:14:19 +0200

    Kommentare hinzugefügt

--- utils/colors.tsx
+++ utils/colors.tsx
@@ -1,11 +1,15 @@
 const colors = {
+    //General Colors
     appBackgroundColor: "#1F2227",
     textColor: "#E5E9F0",
+    //Inputfields
     inputFieldColor: "#4A5160",
     inputFieldIconColor: "#30C5FF",
+    //Buttons
     buttonColor: "#30C5FF",
     buttonIconColor: "#fff",
     buttonTextColor: "#fff",
+    //Online, Offline Status
     onlineColor: "26D07C",
     offlineColor: "#FF4F4F"
 }
