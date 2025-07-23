e28e17482d8ebfc472f0fd882f85952f29ec16bc
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-07-23 20:27:16 +0200

    Die erste ,,Passwort vergessen'' Seite implementiert

--- app/passwordForgotten_1.tsx
+++ app/passwordForgotten_1.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text } from "react-native";
+import { View, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Logo from '@/components/Logo';
 import InputField from '@/components/InputField';
@@ -11,6 +11,8 @@ import universalStyles from '@/utils/universalStyles';
 import colors from '@/utils/colors';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import ProgressBar from '@/components/ProgressBar';
+import BackToLogin from '@/components/BackToLogin';
+import progressBarStyles from '@/utils/progressBarStyles';
 
 export default function PasswordForgotton_1() {
 
@@ -30,20 +32,11 @@ export default function PasswordForgotton_1() {
             <InputField inputFieldHeight={universalStyles.INPUTFIELD_BUTTON_HEIGHT} inputFieldWidth={universalStyles.INPUTFIELD_BUTTON_WIDTH} iconType='Email' placeholder='Email'/>
           </View>
           <Button buttonHeight={buttonStyles.heights.bigButton} buttonWidth={buttonStyles.widths.bigButton} fontSize={fonts.size.bigButton} label='Weiter' iconPosition={buttonStyles.iconRightPositions.bigButton} iconType='Weiter' iconSize={buttonStyles.iconSizes.bigButton}/>
-          <View style={universalStyles.styles.textAfterContent}>
-            <Ionicons name="chevron-back" size={13} color={colors.buttonIcon}/>
-            <Link href={"/passwordForgotten_2"}>Zurück zum Login</Link>
-          </View>
         </View>
-
-        <ProgressBar firstBarColor='#4A5160' secondBarColor='#30C5FF'/>
+        <BackToLogin />
+        <ProgressBar count={3} firstBarColor='#30C5FF' secondBarColor='#4A5160' thirdBarColor='#4A5160' barHeight={progressBarStyles.height} barWidth={progressBarStyles.widths.small} barRadius={progressBarStyles.borderRadius}/>
 
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-
-
-});
