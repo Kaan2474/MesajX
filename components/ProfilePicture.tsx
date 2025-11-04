4738f5747a686f7e77bd860e30fc4d5f51554bae
Kaan Hisiroglu <k.hsrglu@gmx.de>
2025-11-04 17:51:00 +0100

    Höhe, Breite, Radius und Schriftgröße dynamisch anpassbar

--- components/ProfilePicture.tsx
+++ components/ProfilePicture.tsx
@@ -1,72 +1,31 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { Text, View } from 'react-native';
+import profilePictureStyles from '@/utils/profilePictureStyles';
 
 type Props = {
   status?: string,
-  size: string,
+  height: number,
+  width: number,
+  radius: number,
+  fontSize: number,
   initials: string,
 };
 
-export default function ProfilePicture({ status, size, initials }: Props) {
+/*
+Ursprungswerte:
+- small: height, width, borderRadius = 50 / fontSize = 20
+- medium: height, width, borderRadius = 60 / fontSize = 24
+- big: height, width, borderRadius = 100 /fontSize = 40
+*/
 
-  if (size === "small") {
-    return (
-      <View style={[styles.profilePictureContainer, {height: 50, width: 50, borderRadius: 50}]}>
-        <View style={styles.initialsContainer}>
-          <Text style={[styles.initials, {fontSize: 20}]}>{initials}</Text>
-        </View>
-        {!! (status === "online") && <View style={[styles.statusSymbol, {backgroundColor: "#26D07C"}]} />}
-        {!! (status === "offline") && <View style={[styles.statusSymbol, {backgroundColor: "#FF4F4F"}]} />}
-      </View>
-    );
-  }
-
-  else if (size === "medium") {
-    return (
-      <View style={[styles.profilePictureContainer, {height: 60, width: 60, borderRadius: 60}]}>
-        <View style={styles.initialsContainer}>
-          <Text style={[styles.initials, {fontSize: 24}]}>{initials}</Text>
-        </View>
-        {!! (status === "online") && <View style={[styles.statusSymbol, {backgroundColor: "#26D07C"}]} />}
-        {!! (status === "offline") && <View style={[styles.statusSymbol, {backgroundColor: "#FF4F4F"}]} />}
-      </View>
-    );
-  }
-
-  else if (size === "big") {
-    return (
-      <View style={[styles.profilePictureContainer, {height: 100, width: 100, borderRadius: 100}]}>
-        <View style={styles.initialsContainer}>
-          <Text style={[styles.initials, {fontSize: 40}]}>{initials}</Text>
-        </View>
-        {!! (status === "online") && <View style={[styles.statusSymbol, {backgroundColor: "#26D07C"}]} />}
-        {!! (status === "offline") && <View style={[styles.statusSymbol, {backgroundColor: "#FF4F4F"}]} />}
-      </View>
-    );
-  }
+export default function ProfilePicture({ status, height, width, radius, fontSize, initials }: Props) {
 
   return (
-    <></>
-  )
+    <View style={[profilePictureStyles.styles.profilePictureContainer, {height: height, width: width, borderRadius: radius}]}>
+      <View style={profilePictureStyles.styles.initialsContainer}>
+        <Text style={[profilePictureStyles.styles.initials, {fontSize: fontSize}]}>{initials}</Text>
+      </View>
+      {!! (status === "online") && <View style={[profilePictureStyles.styles.statusSymbol, {backgroundColor: "#26D07C"}]} />}
+      {!! (status === "offline") && <View style={[profilePictureStyles.styles.statusSymbol, {backgroundColor: "#FF4F4F"}]} />}
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  profilePictureContainer: {
-    backgroundColor: "#4A5160",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  initialsContainer: {
-    top: 3.5,
-  },
-  initials: {
-    color: "#E5E9F0",
-  },
-  statusSymbol: {
-    height: 7.5,
-    width: 7.5,
-    borderRadius: 7.5,
-    alignSelf: "flex-end",
-    top: 5.5,
-    right: 5,
-  }
-});
